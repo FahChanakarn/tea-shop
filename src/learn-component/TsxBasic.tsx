e3074580d5943ea2d82aedb72a786a9c67edc185
chanakarn <faa.chanakarn@gmail.com>
2026-04-30 17:11:43 +0700

    feat: complete project

--- src/learn-component/TsxBasic.tsx
+++
@@ -1,8 +0,0 @@
-import { requestFormReset } from "react-dom";
-
-// TSXBasic คือชื่อ component
-function TSXBasic() {
-  return <div>hello world!</div>;
-}
-
-export default TSXBasic;
--- src/style-component/Tailwind.tsx
+++
@@ -1,5 +0,0 @@
-function Tailwind() {
-  return <div className="bg-pink-200">Tailwind</div>;
-}
-
-export default Tailwind;
